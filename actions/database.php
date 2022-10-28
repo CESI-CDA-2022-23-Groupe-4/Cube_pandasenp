366669a3d7f827ab19c22953675f0f1762dfa8b3
Yann Morlon <yann25m@gmail.com>
2022-10-28 15:44:16 +0200

    Revert "Initial commit"
    This reverts commit 3ba3b2219643ab234eb471777ecf0216b0cdf6be.

--- actions/database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    try{
-        $bdd = new PDO('mysql:host=localhost;dbname=cube_1_forum;charset=utf8;', 'root', 'root');
-    }catch(PDOException $e){
-        die('Une erreur a été trouvée : ' . $e->getMessage());
-    }
